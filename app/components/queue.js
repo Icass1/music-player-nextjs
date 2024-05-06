2cfc7471cb20e3772163ff430f5ec16019104778
icass <i.cassdolopez@gmail.com>
2024-05-06 19:32:44 +0200

    Added click queue skip

--- app/components/queue.js
+++ app/components/queue.js
@@ -7,12 +7,27 @@ export default function Queue() {
     const {
         queue,
         queueIndex,
+        audio,
+        setCurrentSong,
+        setQueueIndex,
     } = useContext(AudioContext);
+
+    const handleClick = (e) => {
+        let index = Number(e.target.getAttribute('index'));
+
+        console.log(index, queue[index], typeof(index))
+
+        audio.src = `https://api.music.rockhosting.org/api/song/${queue[index].id}`;
+        audio.play()
+
+        setQueueIndex(index);
+        setCurrentSong(queue[index]);
+    }
 
     return (
         <div className='flex flex-col'>
-            {queue?.slice(queueIndex+1).map((item) => (
-                <label className='text-lg fade-out-neutral-50' key={item.id}>{item.title}</label>
+            {queue?.slice(queueIndex+1).map((item, index) => (
+                <label className='ml-1 text-lg fade-out-neutral-50' key={item.id} index={index+queueIndex+1} onClick={handleClick}>{item.title}</label>
             ))}
         </div>
     );

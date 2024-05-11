ffe01ed77140ae2fbee4065b124180b398f15434
icass <i.cassdolopez@gmail.com>
2024-05-11 20:17:57 +0200

    Moved repeated song code to components

---
+++ app/components/songContainer.js
@@ -0,0 +1,122 @@
+import { useContext } from "react";
+import { MediaPlayerContext } from "./audioContext";
+import clsx from "clsx";
+import Image from "next/image";
+import Equalizer from "./equalizer";
+
+export function Song({ type, listSongs, index, song, listId }) {
+
+    const { audio, setCurrentSong, randomQueue, currentSong, currentList, setCurrentList, setQueue, setQueueIndex } = useContext(MediaPlayerContext);
+
+    function handlePlayClick() {
+
+        if (song.in_database == false) { // If song.in_database is undifiend, the actual list is a list from database so all songs are available
+            return;
+        }
+
+        if (currentSong.id == song.id && currentList == listId) {
+            audio.currentTime = 0;
+            audio.play()
+        } else {
+
+            let _listSongs = listSongs.filter(song => {if (song.in_database == false) {return false} else {return true}})
+
+            let index = _listSongs.indexOf(song)
+            let list = _listSongs.slice(index + 1).concat(_listSongs.slice(0, index))
+
+            if (randomQueue) {
+                list.sort(() => Math.random() - 0.5)
+            }
+            audio.src = `https://api.music.rockhosting.org/api/song/${song.id}`;
+            audio.play();
+
+            setQueue([song].concat(list));
+            setQueueIndex(0);
+            setCurrentSong(song);
+            setCurrentList(listId);
+        }
+    };
+
+    return (
+        <div onClick={handlePlayClick}>
+            {type == "Album" ? (
+                <AlbumSong key={index} listSongs={listSongs} song={song} index={index} listId={listId}></AlbumSong>
+            ) : (
+                <PlaylistSong key={index} listSongs={listSongs} song={song} index={index} listId={listId}></PlaylistSong>
+            )}
+        </div>
+    )
+}
+
+function AlbumSong({ index, listSongs, song, listId }) {
+
+    const { currentSong, currentList, isPlaying } = useContext(MediaPlayerContext);
+
+    return (
+        <div className='grid ml-3 mr-3 mt-2 mb-2 items-center cursor-pointer hover:bg-neutral-800 rounded-md h-[50px] gap-2' style={{ gridTemplateColumns: '50px 1fr max-content 60px', gridTemplateRows: '50px' }}>
+
+            {song.id == currentSong.id && isPlaying ? (
+                <Equalizer className='w-full h-full top-0' bar_count={10} bar_gap={1} centered={true} toggleCenter={false}/>
+            ) : (
+                <label className={clsx('text-xl text-neutral-400 text-center cursor-pointer', { 'text-yellow-600': song.id == currentSong.id && currentList == listId })}>{index + 1}</label>
+            )}
+
+            <label className={clsx('relative text-2xl fade-out-neutral-300 min-w-0 max-w-full cursor-pointer', { 'fade-out-yellow-600': song.id == currentSong.id && currentList == listId })}>{song.title}</label>
+            {song.in_database == undefined ? (
+                <label></label>
+            ) : (
+                <Image
+                    src={song.in_database ? ("https://api.music.rockhosting.org/images/database2.webp") : ("https://api.music.rockhosting.org/images/download.svg")}
+                    style={{ filter: 'brightness(0) saturate(100%) invert(32%) sepia(57%) saturate(3843%) hue-rotate(39deg) brightness(86%) contrast(98%)' }}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+            )}
+
+            <label className={clsx('text-xl text-neutral-400', { 'text-yellow-600': song.id == currentSong.id && currentList == listId })}>{song.duration}</label>
+        </div>
+    )
+}
+
+function PlaylistSong({ index, listSongs, song, listId }) {
+
+    const { currentSong, isPlaying, currentList } = useContext(MediaPlayerContext);
+
+    return (
+        <div className='grid gap-x-2 ml-3 mr-3 m-3 cursor-pointer rounded-md items-center hover:bg-neutral-800' style={{ gridTemplateColumns: '50px 3fr 1fr 1fr max-content 60px', gridTemplateRows: '50px' }}>
+
+            <div className='relative h-[50px]'>
+                {song.id == currentSong.id && isPlaying && listId == currentList ? (
+                    <>
+                        <Image className='rounded-md absolute opacity-20' alt={song.title} src={song.cover_url} width={50} height={50} />
+                        <Equalizer className='w-full h-full top-0 absolute' bar_count={10} bar_gap={1} centered={true} toggleCenter={false}></Equalizer>
+                    </>
+                ) : (
+                    <Image className='rounded-md absolute' alt={song.title} src={song.cover_url} width={50} height={50} />
+                )}
+            </div>
+
+            <div className='flex flex-col cursor-pointer min-w-0 max-w-full'>
+                <label className={clsx('cursor-pointer text-xl fade-out-neutral-200 min-w-0 max-w-full', { 'fade-out-yellow-500': song.id == currentSong.id && listId == currentList })}>{song.title}</label>
+                <label className={clsx('cursor-pointer fade-out-neutral-300 min-w-0 max-w-full', { 'fade-out-yellow-600': song.id == currentSong.id && listId == currentList })}>{song.artist}</label>
+            </div>
+            <label className={clsx('fade-out-neutral-200 min-w-0 max-w-full', { 'fade-out-yellow-500': song.id == currentSong.id && listId == currentList })} title={song.genre}>{song.genre}</label>
+            <label className={clsx('fade-out-neutral-200 min-w-0 max-w-full', { 'fade-out-yellow-500': song.id == currentSong.id && listId == currentList })} title={song.album}>{song.album}</label>
+
+            {song.in_database == undefined ? (
+                <label></label>
+            ) : (
+                <Image
+                    src={song.in_database ? ("https://api.music.rockhosting.org/images/database2.webp") : ("https://api.music.rockhosting.org/images/download.svg")}
+                    style={{ filter: 'brightness(0) saturate(100%) invert(32%) sepia(57%) saturate(3843%) hue-rotate(39deg) brightness(86%) contrast(98%)' }}
+                    width={30}
+                    height={30}
+                    alt=""
+                />
+            )}
+
+            <label className={clsx('fade-out-neutral-100 text-xl min-w-0 max-w-full text-center', { 'fade-out-yellow-500': song.id == currentSong.id && listId == currentList})} title={song.duration}>{song.duration}</label>
+        </div>
+    )
+}
--- app/list/[id]/page.js
+++ app/list/[id]/page.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import clsx from 'clsx';
 import { MediaPlayerContext } from '@/app/components/audioContext';
 import Equalizer from '@/app/components/equalizer';
+import { Song } from '@/app/components/songContainer';
 
 export default function ListPage({ params }) {
 
@@ -208,116 +209,8 @@ export default function ListPage({ params }) {
             }
 
             {musicData.songs.map((item, index) => (
-                <Song key={index} type={musicData.type} listSongs={musicData.songs} listId={params.id} song={item} index={index}></Song>
+                <Song key={index} type={musicData.type} listSongs={musicData.songs} listId={params.id} song={item} index={index}/>
             ))}
         </div>
     );
 };
-
-function Song({ type, listSongs, index, song, listId }) {
-
-    return (
-        <div>
-            {type == "Album" ? (
-                <AlbumSong key={index} listSongs={listSongs} song={song} index={index} listId={listId}></AlbumSong>
-            ) : (
-                <PlaylistSong key={index} listSongs={listSongs} song={song} index={index} listId={listId}></PlaylistSong>
-            )}
-        </div>
-    )
-
-}
-
-function AlbumSong({ index, listSongs, song, listId }) {
-
-    const { audio, setCurrentSong, currentSong, setCurrentList, currentList, setQueue, randomQueue, setQueueIndex, isPlaying } = useContext(MediaPlayerContext);
-
-    function handlePlayClick() {
-        if (currentSong.id == song.id && currentList == listId) {
-            audio.currentTime = 0;
-            audio.play()
-        } else {
-
-            let index = listSongs.indexOf(song)
-            let list = listSongs.slice(index + 1).concat(listSongs.slice(0, index))
-
-            if (randomQueue) {
-                list.sort(() => Math.random() - 0.5)
-            }
-
-            audio.src = `https://api.music.rockhosting.org/api/song/${song.id}`;
-            audio.play();
-
-            setQueue([song].concat(list));
-            setQueueIndex(0);
-            setCurrentSong(song);
-            setCurrentList(listId);
-        }
-    };
-
-    return (
-        <div className='grid ml-3 mr-3 mt-2 mb-2 items-center cursor-pointer hover:bg-neutral-800 rounded-md h-[50px] gap-2' style={{ gridTemplateColumns: '50px 1fr 60px', gridTemplateRows: '50px' }} onClick={handlePlayClick}>
-
-            {song.id == currentSong.id && isPlaying ? (
-                <Equalizer className='w-full h-full top-0' bar_count={10} bar_gap={1} centered={true} toggleCenter={false}></Equalizer>
-            ) : (
-                <label className={clsx('text-xl text-neutral-400 text-center', { 'text-yellow-600': song.id == currentSong.id && currentList == listId })}>{index + 1}</label>
-            )}
-
-            <label className={clsx('relative text-2xl fade-out-neutral-300 min-w-0 max-w-full', { 'fade-out-yellow-600': song.id == currentSong.id && currentList == listId })}>{song.title}</label>
-            <label className={clsx('text-xl text-neutral-400', { 'text-yellow-600': song.id == currentSong.id && currentList == listId })}>{song.duration}</label>
-        </div>
-    )
-}
-
-function PlaylistSong({ index, listSongs, song, listId }) {
-
-    const { audio, setCurrentSong, currentSong, randomQueue, setCurrentList, setQueue, setQueueIndex, isPlaying } = useContext(MediaPlayerContext);
-
-    function handlePlayClick() {
-        if (currentSong.id == song.id && currentList == listId) {
-            audio.currentTime = 0;
-            audio.play()
-        } else {
-
-            let index = listSongs.indexOf(song)
-            let list = listSongs.slice(index + 1).concat(listSongs.slice(0, index))
-
-            if (randomQueue) {
-                list.sort(() => Math.random() - 0.5)
-            }
-
-            audio.src = `https://api.music.rockhosting.org/api/song/${song.id}`;
-            audio.play();
-
-            setQueue([song].concat(list));
-            setQueueIndex(0);
-            setCurrentSong(song);
-            setCurrentList(listId);
-        }
-    };
-
-    return (
-        <div className='grid gap-x-2 ml-3 mr-3 m-3 cursor-pointer rounded-md items-center hover:bg-neutral-800' style={{ gridTemplateColumns: '50px 3fr 1fr 1fr 60px', gridTemplateRows: '50px' }} onClick={handlePlayClick}>
-
-            <div className='relative h-[50px]'>
-                {song.id == currentSong.id && isPlaying ? (
-                    <>
-                        <Image className='rounded-md absolute opacity-20' alt={song.title} src={`https://api.music.rockhosting.org/api/song/image/${song.id}_50x50`} width={50} height={50} />
-                        <Equalizer className='w-full h-full top-0 absolute' bar_count={10} bar_gap={1} centered={true} toggleCenter={false}></Equalizer>
-                    </>
-                ) : (
-                    <Image className='rounded-md absolute' alt={song.title} src={`https://api.music.rockhosting.org/api/song/image/${song.id}_50x50`} width={50} height={50} />
-                )}
-            </div>
-
-            <div className='flex flex-col cursor-pointer min-w-0 max-w-full'>
-                <label className={clsx('cursor-pointer text-xl fade-out-neutral-200 min-w-0 max-w-full', { 'fade-out-yellow-500': song.id == currentSong.id })}>{song.title}</label>
-                <label className={clsx('cursor-pointer fade-out-neutral-300 min-w-0 max-w-full', { 'fade-out-yellow-600': song.id == currentSong.id })}>{song.artist}</label>
-            </div>
-            <label className={clsx('fade-out-neutral-200 min-w-0 max-w-full', { 'fade-out-yellow-500': song.id == currentSong.id })} title={song.genre}>{song.genre}</label>
-            <label className={clsx('fade-out-neutral-200 min-w-0 max-w-full', { 'fade-out-yellow-500': song.id == currentSong.id })} title={song.album}>{song.album}</label>
-            <label className={clsx('fade-out-neutral-100 text-xl min-w-0 max-w-full text-center', { 'fade-out-yellow-500': song.id == currentSong.id })} title={song.duration}>{song.duration}</label>
-        </div>
-    )
-}

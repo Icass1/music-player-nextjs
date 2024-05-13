6017e6406413c1a7a4739f80f10a95ccad6e7a7b
icass <i.cassdolopez@gmail.com>
2024-05-13 11:18:38 +0200

    updateDocker.sh will exit if a command returns error

--- updateDocker.sh
+++ updateDocker.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+set -e
 docker buildx build -t music-player -f Dockerfile .
+
 docker stop MusicPlayer
 docker rm MusicPlayer
 docker run --name MusicPlayer -d -t -p 8080:8080 music-player

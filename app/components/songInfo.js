a0b80383cb1e5d9bebba039da67c5f268ab8bd68
icass <i.cassdolopez@gmail.com>
2024-05-30 21:21:02 +0200

    Major changes to adapt to mobile

--- app/components/songInfo.js
+++ app/components/songInfo.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { MediaPlayerContext } from './audioContext';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 // import './globals.css'
 import Slider from './slider';
@@ -20,11 +20,15 @@ export default function SongInfo() {
         queueIndex,
         setCurrentSong,
         setQueueIndex,
+        nextSong,
+        previousSong,
     } = useContext(MediaPlayerContext);
 
     const [sliderValue, setSliderValue] = useState(0);
     const [showingEqualizer, setShowingEqualizer] = useState(false);
     const [innerWidth, setInnerWidth] = useState(0);
+
+    const songDataRef = useRef();
 
     useEffect(() => {
         if (audioDuration == 0) { return }
@@ -83,17 +87,173 @@ export default function SongInfo() {
         }
     }
 
-
     useEffect(() => {
+
+        const handleResize = () => {
+            setInnerWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handleResize)
         setInnerWidth(window.innerWidth)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
     }, [])
 
+    // useEffect(() => {
+    //     songDataRef.current.addEventListener('touchstart', handleTouchStart, false);
+    //     songDataRef.current.addEventListener('touchmove', handleTouchMove, false);
+
+    //     var xDown = null;
+    //     var yDown = null;
+
+    //     function getTouches(evt) {
+    //         // console.log("getTouches", evt)
+    //         return evt.touches ||             // browser API
+    //             evt.originalEvent.touches; // jQuery
+    //     }
+
+    //     function handleTouchStart(evt) {
+    //         // console.log("handleTouchStart", evt)
+    //         const firstTouch = getTouches(evt)[0];
+    //         xDown = firstTouch.clientX;
+    //         yDown = firstTouch.clientY;
+    //     };
+
+    //     function handleTouchMove(evt) {
+    //         // console.log("handleTouchMove", evt)
+    //         if (!xDown || !yDown) {
+    //             return;
+    //         }
+    //         console.log("A")
+
+    //         var xUp = evt.touches[0].clientX;
+    //         var yUp = evt.touches[0].clientY;
+
+    //         var xDiff = xDown - xUp;
+    //         var yDiff = yDown - yUp;
+
+    //         // console.log(xDiff, yDiff)
+
+    //         if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+    //             if (xDiff > 0) {
+    //                 console.log("right")
+    //                 nextSong()
+    //             } else {
+    //                 previousSong()
+    //                 console.log("left")
+    //             }
+    //         } else {
+    //             if (yDiff > 0) {
+    //                 console.log("down")
+    //             } else {
+    //                 console.log("up")
+    //             }
+    //         }
+    //         xDown = null;
+    //         yDown = null;
+    //     };
+
+    //     return () => {
+    //         songDataRef.current.removeEventListener('touchstart', handleTouchStart);
+    //         songDataRef.current.removeEventListener('touchmove', handleTouchMove);
+    //     }
+    // }, [queue, queueIndex, currentTime, songDataRef])
+
+
+    useEffect(() => {
+
+        let xStartPos;
+        let startScrollPos;
+        const handleTouchStart = (event) => {
+            let x = event.touches[0].clientX;
+
+            xStartPos = x
+            startScrollPos = songDataRef.current.scrollLeft
+
+            songDataRef.current.style.scrollBehavior = "unset"
+        }
+
+        const handleTouchMove = (event) => {
+
+            let x = event.touches[0].clientX;
+            songDataRef.current.scrollTo(startScrollPos + xStartPos - x, 0)
+
+        }
+        const handleTouchEnd = (event) => {
+            songDataRef.current.style.scrollBehavior = ""
+
+            console.log("handleTouchEnd", songDataRef.current.scrollLeft - songDataRef.current.offsetWidth, songDataRef.current.offsetWidth / 3)
+
+            if (Math.abs(songDataRef.current.scrollLeft - songDataRef.current.offsetWidth) > songDataRef.current.offsetWidth / 3) {
+
+                if (songDataRef.current.scrollLeft - songDataRef.current.offsetWidth > 0) {
+                    songDataRef.current.scrollTo(songDataRef.current.offsetWidth * 2, 0)
+
+                    // nextSong()
+                    function func() {
+                        console.log(songDataRef.current.offsetWidth * 2, songDataRef.current.scrollLeft)
+                        if (songDataRef.current.scrollLeft == songDataRef.current.offsetWidth * 2) {
+                            songDataRef.current.removeEventListener("scroll", func)
+                            console.log("nextsong 2")
+                            nextSong()
+                        }
+                    }
+                    songDataRef.current.addEventListener("scroll", func)
+
+                    console.log("nextsong 1")
+
+                } else {
+                    songDataRef.current.scrollTo(0, 0)
+
+                    function func() {
+                        console.log(0, songDataRef.current.scrollLeft)
+                        if (songDataRef.current.scrollLeft == 0) {
+                            songDataRef.current.removeEventListener("scroll", func)
+                            console.log("previousSong 2")
+                            previousSong()
+                        }
+                    }
+                    songDataRef.current.addEventListener("scroll", func)
+
+                    console.log("previousSong 1")
+                }
+            } else {
+                songDataRef.current.scrollTo(songDataRef.current.offsetWidth, 0)
+
+            }
+        }
+
+        const handleTouchCancel = () => {
+            songDataRef.current.style.scrollBehavior = ""
+        }
+
+        songDataRef.current.addEventListener('touchstart', handleTouchStart);
+        songDataRef.current.addEventListener('touchmove', handleTouchMove,);
+        songDataRef.current.addEventListener('touchend', handleTouchEnd);
+        songDataRef.current.addEventListener('touchcancel', handleTouchCancel);
+
+        return () => {
+            songDataRef.current.removeEventListener('touchstart', handleTouchStart);
+            songDataRef.current.removeEventListener('touchmove', handleTouchMove);
+            songDataRef.current.removeEventListener('touchend', handleTouchEnd);
+            songDataRef.current.removeEventListener('touchcancel', handleTouchCancel);
+        }
+
+    }, [queue, queueIndex, currentTime, songDataRef])
+
+    useEffect(() => {
+        songDataRef.current.style.scrollBehavior = "unset"
+        songDataRef.current.scrollTo(songDataRef.current.offsetWidth, 0)
+        songDataRef.current.style.scrollBehavior = ""
+    }, [queue, queueIndex, songDataRef])
 
     return (
         <>
             <Image
                 priority="high"
-                className='hidden md:block absolute left-1/2 -translate-x-1/2 h-full w-auto max-w-none blur-sm opacity-60 brightness-50 select-none'
+                className=' md:block absolute top-1/2 md:left-1/2 -translate-y-1/2 md:-translate-x-1/2 h-auto md:h-full w-full md:w-auto max-w-none blur-sm opacity-60 brightness-50 select-none'
                 alt="Current Song"
                 sizes="100vw"
                 src={`https://api.music.rockhosting.org/api/song/image/${currentSong.id == "" ? ("_") : (currentSong.id)}`}
@@ -101,23 +261,35 @@ export default function SongInfo() {
                 height={0}
             />
 
-            <div className='relative grid md:flex md:flex-col z-40 h-full w-full m-0 top-0 items-center' style={{gridTemplateColumns: '60px 60px 1fr', gridTemplateRows: '100%'}}>
-                <div className='md:w-full md:order-none order-3'>
-                    <label className='block text-xl md:text-3xl ml-2 mt-2 mr-2 fade-out-neutral-200 md:min-h-9 font-bold'>{currentSong.title}</label>
-                    <label className='block text-lg md:text-2xl fade-out-neutral-300 ml-2 mr-2 mb-2 md:min-h-8'>{currentSong.artist}</label>
+            <div className='relative grid z-40 h-full w-full m-0 top-0 items-center justify-items-start md:justify-items-center ' style={{ gridTemplateColumns: '60px 1fr 60px', gridTemplateRows: `${innerWidth > 768 ? '80px' : '60px'} 1fr 40px 40px` }}>
+                <div id="asdf" ref={songDataRef} className='w-full absolute  flex flex-row scroll-smooth overflow-hidden  row-start-1 row-end-2     col-start-2 col-end-3     md:row-start-1 md:row-end-2    md:col-start-1 md:col-end-4'>
+                    <div className="flex-shrink-0 w-full">
+                        <label className='block text-xl md:text-3xl ml-2 mt-2 mr-2 fade-out-neutral-200 md:min-h-9 font-bold'>{queue[queueIndex - 1]?.title}</label>
+                        <label className='block text-lg md:text-2xl fade-out-neutral-300 ml-2 mr-2 mb-2 md:min-h-8'>{queue[queueIndex - 1]?.artist}</label>
+                    </div>
+                    <div className="flex-shrink-0 w-full">
+                        <label className='block text-xl md:text-3xl ml-2 mt-2 mr-2 fade-out-neutral-200 md:min-h-9 font-bold'>{queue[queueIndex]?.title}</label>
+                        <label className='block text-lg md:text-2xl fade-out-neutral-300 ml-2 mr-2 mb-2 md:min-h-8'>{queue[queueIndex]?.artist}</label>
+                    </div>
+                    <div className="flex-shrink-0 w-full">
+                        <label className='block text-xl md:text-3xl ml-2 mt-2 mr-2 fade-out-neutral-200 md:min-h-9 font-bold'>{queue[queueIndex + 1]?.title}</label>
+                        <label className='block text-lg md:text-2xl fade-out-neutral-300 ml-2 mr-2 mb-2 md:min-h-8'>{queue[queueIndex + 1]?.artist}</label>
+                    </div>
                 </div>
-                <div className='realtive md:order-none order-1 h-full md:w-[180px] md:h-[180px] flex cursor-pointer' onClick={() => { setShowingEqualizer((prevState) => !prevState) }}>
 
-                    <Image priority="high" className={clsx('realtive select-none h-full w-auto', { "opacity-40": showingEqualizer })} alt="Current Song" src={`https://api.music.rockhosting.org/api/song/image/${currentSong.id == "" ? ("_") : (currentSong.id)}`} width={180} height={180} />
+                <div className='realtive   row-start-1 row-end-2     col-start-1 col-end-2    md:row-start-2 md:row-end-3     md:col-start-1 md:col-end-4    p-1 md:p-0  h-full md:w-[180px] md:h-[180px] flex cursor-pointer' onClick={() => { setShowingEqualizer((prevState) => !prevState) }}>
+
+                    <Image priority="high" className={clsx('realtive select-none h-full w-auto rounded-lg md:rounded-none', { "opacity-40": showingEqualizer })} alt="Current Song" src={`https://api.music.rockhosting.org/api/song/image/${currentSong.id == "" ? ("_") : (currentSong.id)}`} width={180} height={180} />
 
                     {showingEqualizer ? (
-                        <Equalizer bar_gap={0} bar_count={innerWidth > 768 ? 180 : 60} toggleCenter={false} centered={true} className='select-none w-[60px] h-[60px] absolute md:w-[180px] md:h-[180px]' />
+                        <Equalizer bar_gap={0} bar_count={innerWidth > 768 ? 180 : 52} toggleCenter={false} centered={true} className='select-none w-[52px] h-[52px] absolute md:w-[180px] md:h-[180px]' />
+                        // <Equalizer bar_gap={0} bar_count={innerWidth > 768 ? 180 : 60} toggleCenter={false} centered={true} className='select-none w-[60px] h-[60px] absolute md:w-[180px] md:h-[180px]' />
                     ) : (
                         <></>
                     )}
                 </div>
 
-                <div className='bottom-1 md:order-none order-2'>
+                <div className='bottom-1     row-start-1 row-end-2    col-start-3 col-end-4     md:row-start-3 md:row-end-4    md:col-start-1 md:col-end-4'>
                     <div className='grid items-center justify-items-center ml-auto mr-auto' style={{ gridTemplateColumns: innerWidth > 768 ? '30px 60px 30px' : '60px' }}>
                         <Image alt="Previous" className='hidden md:block invert-[0.8] hover:invert-[0.9] cursor-pointer' src='https://api.music.rockhosting.org/images/previous.svg' width={30} height={30} onClick={handlePrevious} />
                         {isPlaying ?
@@ -128,7 +300,8 @@ export default function SongInfo() {
                         <Image alt="Next" className='hidden md:block invert-[0.8] hover:invert-[0.9] cursor-pointer' src='https://api.music.rockhosting.org/images/next.svg' width={30} height={30} onClick={handleNext} />
                     </div>
                 </div>
-                <div className='hidden md:block md:order-none'>
+
+                <div className='hidden md:block    md:row-start-4 md:row-end-5   md:col-start-1 md:col-end-4'>
                     <div className='hidden md:grid relative gap-1 ml-2 mr-2 items-center justify-items-center' style={{ gridTemplateColumns: '50px 1fr 50px' }}>
                         <label>{getTime(currentTime)}</label>
                         <Slider value={sliderValue} onInput={sliderInput} onChange={sliderChange}></Slider>

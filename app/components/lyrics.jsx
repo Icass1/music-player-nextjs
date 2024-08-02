2ea40596f0e64b2f3fed2c34175cc8ded89259b2
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-08-02 13:37:56 +0200

    Added follow dynamic lyrics and split segments by commas

--- app/components/lyrics.jsx
+++ app/components/lyrics.jsx
@@ -1,6 +1,7 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { MediaPlayerContext } from "./audioContext";
 import clsx from "clsx";
+import { debounce } from "lodash";
 
 
 function DynamicLyrics(lyrics) {
@@ -15,15 +16,10 @@ function DynamicLyrics(lyrics) {
             </div>
         )
     }
-
-
-
-    console.log(currentTime)
-
     return (
         <>
             {lyrics.lyrics.segments.map((line, index) =>
-                <div key={"line" + index} className="w-fit ml-auto mr-auto">
+                <div key={"line" + index} id={"segment-id-" + line.id} className="w-fit ml-auto mr-auto">
                     {line.words.map((word, index) =>
                         <label key={"word" + index} className={clsx("text-center ml-1", { 'text-neutral-100': word.start < currentTime, 'text-neutral-500': word.start > currentTime })}>{word.text}</label>
                     )}
@@ -47,7 +43,6 @@ function NormalLyrics(lyrics) {
             </div>
         )
     }
-
     return (
         <>
             {lyrics.lyrics.split("\n").map((line, index) =>
@@ -64,21 +59,66 @@ export default function Lyrics() {
 
     const {
         currentSong,
+        currentTime,
     } = useContext(MediaPlayerContext);
 
     const [dynamicLyrics, setDynamicLyrics] = useState(false)
     const [lyrics, setLyrics] = useState()
+    const mainRef = useRef();
+
+    const [followLyrics, setFollowLyrics] = useState(true)
 
     useEffect(() => {
         if (!currentSong.id) {
             return
         }
 
-        fetch(`https://api.music.rockhosting.org/api/song/lyrics/${currentSong.id}`).then(reponse => {
-            if (reponse.status == 200) {
-                reponse.json().then(response => {
-                    setDynamicLyrics(response.has_dynamic_lyrics)
-                    setLyrics(response.lyrics)
+        fetch(`https://api.music.rockhosting.org/api/song/lyrics/${currentSong.id}`).then(response => {
+            if (response.status == 200) {
+                response.json().then(data => {
+
+                    let lyricsOut;
+
+                    if (data.has_dynamic_lyrics && data.lyrics) {
+                        lyricsOut = { segments: [] }
+                        let segmentIndex = 0
+
+
+                        for (let segment of data.lyrics.segments) {
+
+                            let newSegment = { id: segmentIndex, words: [] }
+                            segmentIndex++;
+
+                            for (let word of segment.words) {
+
+                                if (word.text.includes(",")) {
+                                    word.text = word.text.replace(",", "")
+                                    newSegment.words.push(word)
+                                    newSegment.end = word.end
+                                    lyricsOut.segments.push(newSegment)
+
+                                    newSegment = { id: segmentIndex, words: [] }
+                                    segmentIndex++;
+                                } else {
+                                    if (newSegment.words.length == 0) {
+                                        newSegment.start = word.start
+                                        word.text = word.text[0].toUpperCase() + word.text.slice(1,)
+                                    }
+                                    newSegment.words.push(word)
+                                }
+                            }
+                            newSegment.end = segment.end
+                            lyricsOut.segments.push(newSegment)
+
+                        }
+                        console.log(lyricsOut)
+                    }
+                    setDynamicLyrics(data.has_dynamic_lyrics)
+                    if (lyricsOut) {
+                        setLyrics(lyricsOut)
+                    } else {
+                        setLyrics(data.lyrics)
+                    }
                 })
             } else {
                 setDynamicLyrics(false)
@@ -88,9 +128,45 @@ export default function Lyrics() {
 
     }, [currentSong])
 
+    useEffect(() => {
+        if (!dynamicLyrics) { return }
+        if (!followLyrics) { return }
+
+        for (let segmentIndex in lyrics.segments) {
+            let segment = lyrics.segments[segmentIndex];
+
+            if (segment.start < currentTime && segment.end > currentTime) {
+                let scroll = document.querySelector("#segment-id-" + segment.id).offsetTop - mainRef.current.parentNode.offsetTop
+
+                mainRef.current.parentNode.scrollTo(0, scroll - mainRef.current.parentNode.offsetHeight / 2)
+                break
+            }
+        }
+    }, [dynamicLyrics, lyrics, currentTime, mainRef, followLyrics])
+
+    useEffect(() => {
+
+        const element = mainRef.current
+
+        const timeoutFollowLyrics = debounce((e) => {
+            setFollowLyrics(true)
+        }, 4000);
+
+        const handleScroll = () => {
+            setFollowLyrics(false)
+            timeoutFollowLyrics()
+        }
+
+        element.parentNode.addEventListener("scroll", handleScroll)
+
+        return () => {
+            element.parentNode.removeEventListener("scroll", handleScroll)
+        }
+
+    }, [mainRef])
 
     return (
-        <div className="flex flex-col h-full text-lg">
+        <div ref={mainRef} className="flex flex-col h-full text-lg">
             <div className="min-h-6 list-item"></div>
             {dynamicLyrics ? <DynamicLyrics lyrics={lyrics} /> : <NormalLyrics lyrics={lyrics} />}
             <div className="min-h-20 list-item"></div>

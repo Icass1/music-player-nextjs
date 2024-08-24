1410e0d76c03d431088f6f73b0815c3fdcf1ed70
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-08-24 13:28:55 +0200

    Added loading and error messages

--- app/list/[id]/page.js
+++ app/list/[id]/page.js
@@ -19,21 +19,37 @@ export default function ListPage({ params }) {
         const fetchData = async () => {
             const response = await apiFetch(`/api/list/${params.id}`);
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                setMusicData({ 'error': true, 'status': response.status, 'statusText': response.statusText });
+                return
             }
             const data = await response.json();
             if (data.type == "Album") {
                 data.songs = data.songs.map(song => Object.assign({}, song, { "search": song.title.toUpperCase() }))
             } else {
-                data.songs = data.songs.map(song => Object.assign({}, song, { "search": song.title.toUpperCase() + " " +  song.artist.toUpperCase() + " "  + song.album.toUpperCase() + " "  + song.genre.toUpperCase()}))
+                data.songs = data.songs.map(song => Object.assign({}, song, { "search": song.title.toUpperCase() + " " + song.artist.toUpperCase() + " " + song.album.toUpperCase() + " " + song.genre.toUpperCase() }))
             }
-            
+
             setMusicData(data);
         };
         fetchData();
     }, [params.id]);
 
+    if (musicData?.error === true) {
+        return (
+            <div className='w-fit h-fit relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col' >
+                {musicData.status === 404 ?
+                    <label className='text-center text-4xl font-bold text-neutral-100'>List not found</label>
+                    :
+                    <></>
+                }
+                <label className='text-center text-xs'>{musicData.status} {musicData.statusText}</label>
+            </div >
+        )
+    } else if (musicData?.id === '') {
+        return <label className='w-fit h-fit relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col text-center text-4xl font-bold text-neutral-100'>Loading...</label>
+    }
+
     return (
-        <DefaultListPage listId={params.id} musicData={musicData} setMusicData={setMusicData}/>
+        <DefaultListPage listId={params.id} musicData={musicData} setMusicData={setMusicData} />
     )
 }
